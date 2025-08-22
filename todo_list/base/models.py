f6096cae8a6d2960625da920311ede20025c6daf
Mohamed Fouad <mfb4010@gmail.com>
2025-08-22 20:52:17 +0400

    first

---
+++ todo_list/base/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Task(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    message = models.CharField(max_length=200)
+    description = models.TextField(null=True,blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    comleted = models.BooleanField(default=False)
+
+    # class Meta:
+    #     ordering = ['updated','created'] 
+
+    def __str__(self):
+        return self.message[0:51]
+    
